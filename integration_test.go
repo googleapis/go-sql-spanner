fbc272c087e2929414949ff54d50898dacb01376
Knut Olav Løite <koloite@gmail.com>
2021-08-10 11:44:48 +0200

    build: run integration tests on production

--- integration_test.go
+++ integration_test.go
@@ -51,13 +51,6 @@ func init() {
 	if projectId, ok = os.LookupEnv("SPANNER_TEST_PROJECT"); !ok {
 		projectId = "test-project"
 	}
-
-	// Automatically create test instance on the emulator if necessary.
-	if _, ok = os.LookupEnv("SPANNER_EMULATOR_HOST"); ok {
-		if err := initEmulator(); err != nil {
-			log.Fatal(err)
-		}
-	}
 }
 
 func runsOnEmulator() bool {
@@ -67,36 +60,54 @@ func runsOnEmulator() bool {
 	return false
 }
 
-func initEmulator() error {
+func initTestInstance(config string) (cleanup func(), err error) {
 	ctx := context.Background()
 	instanceAdmin, err := instance.NewInstanceAdminClient(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer instanceAdmin.Close()
+	// Check if the instance exists or not.
+	_, err = instanceAdmin.GetInstance(ctx, &instancepb.GetInstanceRequest{
+		Name: fmt.Sprintf("projects/%s/instances/%s", projectId, instanceId),
+	})
+	if err == nil {
+		return func() {}, nil
+	}
+	if spanner.ErrCode(err) != codes.NotFound {
+		return nil, err
+	}
+
+	// Instance does not exist. Create a temporary instance for this test run.
+	// The instance will be deleted after the test run.
 	op, err := instanceAdmin.CreateInstance(ctx, &instancepb.CreateInstanceRequest{
 		Parent:     fmt.Sprintf("projects/%s", projectId),
 		InstanceId: instanceId,
 		Instance: &instancepb.Instance{
-			Config:      fmt.Sprintf("projects/%s/instanceConfigs/%s", projectId, "emulator-config"),
+			Config:      fmt.Sprintf("projects/%s/instanceConfigs/%s", projectId, config),
 			DisplayName: instanceId,
 			NodeCount:   1,
 		},
 	})
 	if err != nil {
-		// Check if the instance has already been created by another (parallel) test.
-		code := spanner.ErrCode(err)
-		if code != codes.AlreadyExists {
-			return fmt.Errorf("could not create instance %s: %v", fmt.Sprintf("projects/%s/instances/%s", projectId, instanceId), err)
-		}
+		return nil, fmt.Errorf("could not create instance %s: %v", fmt.Sprintf("projects/%s/instances/%s", projectId, instanceId), err)
 	} else {
 		// Wait for the instance creation to finish.
 		_, err := op.Wait(ctx)
 		if err != nil {
-			return fmt.Errorf("waiting for instance creation to finish failed: %v", err)
+			return nil, fmt.Errorf("waiting for instance creation to finish failed: %v", err)
 		}
 	}
-	return nil
+	// Delete the instance after all tests have finished.
+	return func() {
+		instanceAdmin, err := instance.NewInstanceAdminClient(ctx)
+		if err != nil {
+			return
+		}
+		instanceAdmin.DeleteInstance(ctx, &instancepb.DeleteInstanceRequest{
+			Name: fmt.Sprintf("projects/%s/instances/%s", projectId, instanceId),
+		})
+	}, nil
 }
 
 func createTestDB(ctx context.Context, statements ...string) (dsn string, cleanup func(), err error) {
@@ -139,19 +150,34 @@ func createTestDB(ctx context.Context, statements ...string) (dsn string, cleanu
 	return
 }
 
-func initIntegrationTests() (cleanup func()) {
+func initIntegrationTests() (cleanup func(), err error) {
 	flag.Parse() // Needed for testing.Short().
 	noop := func() {}
 
 	if testing.Short() {
 		log.Println("Integration tests skipped in -short mode.")
-		return noop
+		return noop, nil
 	}
-	return noop
+
+	// Automatically create test instance if necessary.
+	config := "regional-us-east1"
+	if _, ok := os.LookupEnv("SPANNER_EMULATOR_HOST"); ok {
+		config = "emulator-config"
+	}
+	cleanup, err = initTestInstance(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return cleanup, nil
 }
 
 func TestMain(m *testing.M) {
-	cleanup := initIntegrationTests()
+	cleanup, err := initIntegrationTests()
+	if err != nil {
+		log.Fatalf("could not init integration tests: %v", err)
+		os.Exit(1)
+	}
 	res := m.Run()
 	cleanup()
 	os.Exit(res)
