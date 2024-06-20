cc4d789087dfccfc4458beaa268216d99979bfdb
Egon Elbre <egonelbre@gmail.com>
2024-06-20 13:45:41 +0300

    fix(benchmarks): fix formatting argument

--- benchmarks/benchwrapper/main.go
+++ benchmarks/benchwrapper/main.go
@@ -119,7 +119,7 @@ func (s *server) Update(ctx context.Context, req *pb.UpdateQuery) (*pb.EmptyResp
 	for _, q := range req.Queries {
 		_, err = tx.ExecContext(ctx, q)
 		if err != nil {
-			log.Fatalf("failed to execute query: v", err)
+			log.Fatalf("failed to execute query: %v", err)
 		}
 	}
 	// Run the active DML batch.
