0e2f9acba6f156a7bdb08c66c3bfe4f08cc77e06
Knut Olav Løite <koloite@gmail.com>
2025-08-06 17:49:43 +0200

    docs: add snippets for PostgreSQL

--- snippets/getting_started_guide_test.go
+++ snippets/getting_started_guide_test.go
@@ -19,14 +19,19 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"testing"
+	"time"
 
 	database "cloud.google.com/go/spanner/admin/database/apiv1"
 	adminpb "cloud.google.com/go/spanner/admin/database/apiv1/databasepb"
 	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
 	"cloud.google.com/go/spanner/admin/instance/apiv1/instancepb"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 	"github.com/googleapis/go-sql-spanner/examples/samples"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -38,7 +43,7 @@ func TestSamples(t *testing.T) {
 	databaseID := "test-database"
 	databaseName := fmt.Sprintf("projects/%s/instances/%s/databases/%s", projectID, instanceID, databaseID)
 
-	emulator, err := startEmulator(projectID, instanceID, databaseID)
+	emulator, err := startEmulator(projectID, instanceID, databaseID, adminpb.DatabaseDialect_GOOGLE_STANDARD_SQL)
 	if err != nil {
 		if emulator != nil {
 			emulator.Terminate(context.Background())
@@ -69,6 +74,53 @@ func TestSamples(t *testing.T) {
 	testSample(t, ctx, &b, databaseName, samples.PartitionedDml, "PDML", "Updated at least 3 albums\n")
 }
 
+func TestPostgreSQLSamples(t *testing.T) {
+	projectID := "emulator-project"
+	instanceID := "test-instance"
+	databaseID := "test-database"
+	databaseName := fmt.Sprintf("projects/%s/instances/%s/databases/%s", projectID, instanceID, databaseID)
+
+	//emulator, err := startEmulator(projectID, instanceID, databaseID)
+	//if err != nil {
+	//	if emulator != nil {
+	//		_ = emulator.Terminate(context.Background())
+	//	}
+	//	t.Fatalf("failed to start emulator: %v", err)
+	//}
+	//defer emulator.Terminate(context.Background())
+	if err := startEmulatorPg(); err != nil {
+		t.Fatal(err)
+	}
+	defer stopEmulator()
+	if err := createInstance(projectID, instanceID); err != nil {
+		t.Fatalf("failed to create instance: %v", err)
+	}
+	if err := createDatabase(projectID, instanceID, databaseID, adminpb.DatabaseDialect_POSTGRESQL); err != nil {
+		t.Fatalf("failed to create database: %v", err)
+	}
+
+	ctx := context.Background()
+	var b bytes.Buffer
+
+	testSample(t, ctx, &b, databaseName, samples.CreateTablesPostgreSQL, "CreateTablesPostgreSQL", fmt.Sprintf("Created singers & albums tables in database: [%s]\n", databaseName))
+	testSample(t, ctx, &b, databaseName, samples.CreateConnectionPostgreSQL, "CreateConnectionPostgreSQL", "Greeting from Spanner PostgreSQL: Hello world!\n")
+	testSample(t, ctx, &b, databaseName, samples.WriteDataWithDmlPostgreSQL, "WriteDataWithDmlPostgreSQL", "4 records inserted\n")
+	testSample(t, ctx, &b, databaseName, samples.WriteDataWithDmlBatchPostgreSQL, "WriteDataWithDmlBatchPostgreSQL", "3 records inserted\n")
+	testSample(t, ctx, &b, databaseName, samples.WriteDataWithMutationsPostgreSQL, "WriteDataWithMutationsPostgreSQL", "Inserted 10 rows\n")
+	testSample(t, ctx, &b, databaseName, samples.QueryDataPostgreSQL, "QueryDataPostgreSQL", "1 1 Total Junk\n1 2 Go, Go, Go\n2 1 Green\n2 2 Forever Hold Your Peace\n2 3 Terrified\n")
+	testSample(t, ctx, &b, databaseName, samples.QueryDataWithParameterPostgreSQL, "QueryDataWithParameterPostgreSQL", "12 Melissa Garcia\n")
+	testSample(t, ctx, &b, databaseName, samples.QueryDataWithTimeoutPostgreSQL, "QueryDataWithTimeoutPostgreSQL", "")
+	testSample(t, ctx, &b, databaseName, samples.AddColumnPostgreSQL, "AddColumnPostgreSQL", "Added marketing_budget column\n")
+	testSample(t, ctx, &b, databaseName, samples.DdlBatchPostgreSQL, "DdlBatchPostgreSQL", "Added venues and concerts tables\n")
+	testSample(t, ctx, &b, databaseName, samples.UpdateDataWithMutationsPostgreSQL, "UpdateDataWithMutationsPostgreSQL", "Updated 2 albums\n")
+	testSample(t, ctx, &b, databaseName, samples.QueryNewColumnPostgreSQL, "QueryNewColumnPostgreSQL", "1 1 100000\n1 2 null\n2 1 null\n2 2 500000\n2 3 null\n")
+	testSample(t, ctx, &b, databaseName, samples.WriteWithTransactionUsingDmlPostgreSQL, "WriteWithTransactionUsingDmlPostgreSQL", "Transferred marketing budget from Album 2 to Album 1\n")
+	testSample(t, ctx, &b, databaseName, samples.TagsPostgreSQL, "TagsPostgreSQL", "Reduced marketing budget\n")
+	testSample(t, ctx, &b, databaseName, samples.ReadOnlyTransactionPostgreSQL, "ReadOnlyTransactionPostgreSQL", "1 1 Total Junk\n1 2 Go, Go, Go\n2 1 Green\n2 2 Forever Hold Your Peace\n2 3 Terrified\n2 2 Forever Hold Your Peace\n1 2 Go, Go, Go\n2 1 Green\n2 3 Terrified\n1 1 Total Junk\n")
+	testSample(t, ctx, &b, databaseName, samples.DataBoostPostgreSQL, "DataBoostPostgreSQL", "1 Marc Richards\n2 Catalina Smith\n3 Alice Trentor\n4 Lea Martin\n5 David Lomond\n12 Melissa Garcia\n13 Russel Morales\n14 Jacqueline Long\n15 Dylan Shaw\n16 Sarah Wilson\n17 Ethan Miller\n18 Maya Patel\n")
+	testSample(t, ctx, &b, databaseName, samples.PartitionedDmlPostgreSQL, "PDMLPostgreSQL", "Updated at least 3 albums\n")
+}
+
 func testSample(t *testing.T, ctx context.Context, b *bytes.Buffer, databaseName string, sample func(ctx context.Context, w io.Writer, databaseName string) error, sampleName, want string) {
 	if err := sample(ctx, b, databaseName); err != nil {
 		t.Fatalf("failed to run %s: %v", sampleName, err)
@@ -79,13 +131,83 @@ func testSample(t *testing.T, ctx context.Context, b *bytes.Buffer, databaseName
 	b.Reset()
 }
 
-func startEmulator(projectID, instanceID, databaseID string) (testcontainers.Container, error) {
+var cli *client.Client
+var containerId string
+
+func startEmulatorPg() error {
+	ctx := context.Background()
+	if err := os.Setenv("SPANNER_EMULATOR_HOST", "localhost:9010"); err != nil {
+		return err
+	}
+
+	// Initialize a Docker client.
+	var err error
+	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+	// Pull the Spanner Emulator docker image.
+	reader, err := cli.ImagePull(ctx, "gcr.io/cloud-spanner-emulator/emulator", image.PullOptions{})
+	if err != nil {
+		return err
+	}
+	defer func() { _ = reader.Close() }()
+	// cli.ImagePull is asynchronous.
+	// The reader needs to be read completely for the pull operation to complete.
+	if _, err := io.Copy(io.Discard, reader); err != nil {
+		return err
+	}
+
+	// Create and start a container with the emulator.
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:        "gcr.io/cloud-spanner-emulator/emulator",
+		ExposedPorts: nat.PortSet{"9010": {}},
+	}, &container.HostConfig{
+		AutoRemove:   true,
+		PortBindings: map[nat.Port][]nat.PortBinding{"9010": {{HostIP: "0.0.0.0", HostPort: "9010"}}},
+	}, nil, nil, "")
+	if err != nil {
+		return err
+	}
+	containerId = resp.ID
+	if err := cli.ContainerStart(ctx, containerId, container.StartOptions{}); err != nil {
+		return err
+	}
+	// Wait max 10 seconds or until the emulator is running.
+	for c := 0; c < 20; c++ {
+		// Always wait at least 500 milliseconds to ensure that the emulator is actually ready, as the
+		// state can be reported as ready, while the emulator (or network interface) is actually not ready.
+		<-time.After(500 * time.Millisecond)
+		resp, err := cli.ContainerInspect(ctx, containerId)
+		if err != nil {
+			return fmt.Errorf("failed to inspect container state: %v", err)
+		}
+		if resp.State.Running {
+			break
+		}
+	}
+
+	return nil
+}
+
+func stopEmulator() {
+	if cli == nil || containerId == "" {
+		return
+	}
+	ctx := context.Background()
+	timeout := 10
+	if err := cli.ContainerStop(ctx, containerId, container.StopOptions{Timeout: &timeout}); err != nil {
+		log.Printf("failed to stop emulator: %v\n", err)
+	}
+}
+
+func startEmulator(projectID, instanceID, databaseID string, dialect adminpb.DatabaseDialect) (testcontainers.Container, error) {
 	ctx := context.Background()
 	req := testcontainers.ContainerRequest{
 		AlwaysPullImage: true,
 		Image:           "gcr.io/cloud-spanner-emulator/emulator",
-		ExposedPorts:    []string{"9010/tcp"},
-		WaitingFor:      wait.ForListeningPort("9010/tcp"),
+		ExposedPorts:    []string{"9010:9010"},
+		WaitingFor:      wait.ForListeningPort("9010"),
 		HostConfigModifier: func(hostConfig *container.HostConfig) {
 			hostConfig.AutoRemove = true
 		},
@@ -95,7 +217,7 @@ func startEmulator(projectID, instanceID, databaseID string) (testcontainers.Con
 		Started:          true,
 	})
 	if err != nil {
-		return emulator, fmt.Errorf("failed to start PGAdapter: %v", err)
+		return emulator, fmt.Errorf("failed to start emulator: %v", err)
 	}
 	host, err := emulator.Host(ctx)
 	if err != nil {
@@ -113,7 +235,7 @@ func startEmulator(projectID, instanceID, databaseID string) (testcontainers.Con
 	if err := createInstance(projectID, instanceID); err != nil {
 		return emulator, fmt.Errorf("failed to create instance: %v", err)
 	}
-	if err := createDatabase(projectID, instanceID, databaseID); err != nil {
+	if err := createDatabase(projectID, instanceID, databaseID, dialect); err != nil {
 		return emulator, fmt.Errorf("failed to create database: %v", err)
 	}
 	return emulator, nil
@@ -148,17 +270,24 @@ func createInstance(projectID, instanceID string) error {
 	return nil
 }
 
-func createDatabase(projectID, instanceID, databaseID string) error {
+func createDatabase(projectID, instanceID, databaseID string, dialect adminpb.DatabaseDialect) error {
 	ctx := context.Background()
 	adminClient, err := database.NewDatabaseAdminClient(ctx)
 	if err != nil {
 		return err
 	}
-	defer adminClient.Close()
+	defer func() { _ = adminClient.Close() }()
 
+	var createStatement string
+	if dialect == adminpb.DatabaseDialect_POSTGRESQL {
+		createStatement = fmt.Sprintf(`CREATE DATABASE "%s"`, databaseID)
+	} else {
+		createStatement = fmt.Sprintf("CREATE DATABASE `%s`", databaseID)
+	}
 	op, err := adminClient.CreateDatabase(ctx, &adminpb.CreateDatabaseRequest{
 		Parent:          fmt.Sprintf("projects/%s/instances/%s", projectID, instanceID),
-		CreateStatement: fmt.Sprintf("CREATE DATABASE `%s`", databaseID),
+		CreateStatement: createStatement,
+		DatabaseDialect: dialect,
 	})
 	if err != nil {
 		return err
---
+++ snippets/samples/tags_pg.go
@@ -0,0 +1,83 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package samples
+
+// [START spanner_transaction_and_statement_tag]
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/spanner"
+	spannerdriver "github.com/googleapis/go-sql-spanner"
+)
+
+func TagsPostgreSQL(ctx context.Context, w io.Writer, databaseName string) error {
+	db, err := sql.Open("spanner", databaseName)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = db.Close() }()
+
+	// Use the spannerdriver.BeginReadWriteTransaction function
+	// to specify specific Spanner options, such as transaction tags.
+	tx, err := spannerdriver.BeginReadWriteTransaction(ctx, db,
+		spannerdriver.ReadWriteTransactionOptions{
+			TransactionOptions: spanner.TransactionOptions{
+				TransactionTag: "example-tx-tag",
+			},
+		})
+	if err != nil {
+		return err
+	}
+
+	// Pass in an argument of type spannerdriver.ExecOptions to supply
+	// additional options for a statement.
+	row := tx.QueryRowContext(ctx, "select marketing_budget "+
+		"from albums "+
+		"where singer_id=? and album_id=?",
+		spannerdriver.ExecOptions{
+			QueryOptions: spanner.QueryOptions{RequestTag: "query-marketing-budget"},
+		}, 1, 1)
+	var budget int64
+	if err := row.Scan(&budget); err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
+	// Reduce the marketing budget by 10% if it is more than 1,000.
+	if budget > 1000 {
+		budget = int64(float64(budget) - float64(budget)*0.1)
+		if _, err := tx.ExecContext(ctx,
+			`update albums set marketing_budget=$1 
+               where singer_id=$2 and album_id=$3`,
+			spannerdriver.ExecOptions{
+				QueryOptions: spanner.QueryOptions{RequestTag: "reduce-marketing-budget"},
+			}, budget, 1, 1); err != nil {
+			_ = tx.Rollback()
+			return err
+		}
+	}
+	// Commit the current transaction.
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	_, _ = fmt.Fprintln(w, "Reduced marketing budget")
+
+	return nil
+}
+
+// [END spanner_transaction_and_statement_tag]
