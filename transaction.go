afacb37a605db42c0c7252eb924ed7402a41a9a0
Knut Olav Løite <koloite@gmail.com>
2025-09-08 13:28:13 +0200

    chore: use exported BatchType from parser package
    Use the exported BatchType from the parser package instead of an unexported type.
    This will allow us in the future to export functions to determine the batch type.

--- transaction.go
+++ transaction.go
@@ -536,7 +536,7 @@ func (tx *readWriteTransaction) StartBatchDML(options spanner.QueryOptions, auto
 	}
 	tx.logger.Debug("starting dml batch in transaction", "automatic", automatic)
 	tx.active = true
-	tx.batch = &batch{tp: dml, options: &ExecOptions{QueryOptions: options}, automatic: automatic}
+	tx.batch = &batch{tp: parser.BatchTypeDml, options: &ExecOptions{QueryOptions: options}, automatic: automatic}
 	return driver.ResultNoRows, nil
 }
 
@@ -545,9 +545,9 @@ func (tx *readWriteTransaction) RunBatch(ctx context.Context) (driver.Result, er
 		return nil, spanner.ToSpannerError(status.Errorf(codes.FailedPrecondition, "This transaction does not have an active batch"))
 	}
 	switch tx.batch.tp {
-	case dml:
+	case parser.BatchTypeDml:
 		return tx.runDmlBatch(ctx)
-	case ddl:
+	case parser.BatchTypeDdl:
 		fallthrough
 	default:
 		return nil, spanner.ToSpannerError(status.Errorf(codes.InvalidArgument, "Unknown or unsupported batch type: %d", tx.batch.tp))
@@ -558,7 +558,7 @@ func (tx *readWriteTransaction) RunDmlBatch(ctx context.Context) (SpannerResult,
 	if tx.batch == nil {
 		return nil, spanner.ToSpannerError(status.Errorf(codes.FailedPrecondition, "this transaction does not have an active batch"))
 	}
-	if tx.batch.tp != dml {
+	if tx.batch.tp != parser.BatchTypeDml {
 		return nil, spanner.ToSpannerError(status.Errorf(codes.FailedPrecondition, "batch is not a DML batch"))
 	}
 	return tx.runDmlBatch(ctx)
