7716c72e094ca331e145d97d33008e0c03d5e0d7
Knut Olav Løite <koloite@gmail.com>
2025-10-16 12:52:26 +0200

    chore: add signal handler to remove temp file

--- spannerlib/grpc-server/server.go
+++ spannerlib/grpc-server/server.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"cloud.google.com/go/spanner/apiv1/spannerpb"
 	"google.golang.org/grpc"
@@ -23,9 +25,22 @@ func main() {
 	if len(os.Args) > 2 {
 		tp = os.Args[2]
 	}
+	//
 	if tp == "unix" {
 		defer func() { _ = os.Remove(name) }()
+		// Set up a channel to listen for OS signals that terminate the process,
+		// so we can clean up the temp file in those cases as well.
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		go func() {
+			// Wait for a signal.
+			<-sigs
+			// Delete the temp file.
+			_ = os.Remove(name)
+			os.Exit(0)
+		}()
 	}
+
 	lis, err := net.Listen(tp, name)
 	if err != nil {
 		log.Fatalf("failed to listen: %v\n", err)
