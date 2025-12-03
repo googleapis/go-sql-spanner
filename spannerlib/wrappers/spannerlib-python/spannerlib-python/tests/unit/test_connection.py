952cf958504ba553b452f425bfb0781c466c9a79
Sanjeev Bhatt <sanjeevbhatt@google.com>
2025-12-03 07:21:58 +0000

    refactor: Reorder SpannerLibError import in test_connection.py

--- spannerlib/wrappers/spannerlib-python/spannerlib-python/tests/unit/test_connection.py
+++ spannerlib/wrappers/spannerlib-python/spannerlib-python/tests/unit/test_connection.py
@@ -11,12 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from google.cloud.spannerlib.internal.errors import SpannerLibError
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
 
 from google.cloud.spannerlib import Connection  # type: ignore
+from google.cloud.spannerlib.internal.errors import SpannerLibError
 
 
 class TestConnection:
