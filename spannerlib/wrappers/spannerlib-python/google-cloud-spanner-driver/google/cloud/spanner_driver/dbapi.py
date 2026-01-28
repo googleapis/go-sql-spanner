209f3029c8d691643aa286a3ad07bd6502e2627d
Sanjeev Bhatt <sanjeevbhatt@google.com>
2026-01-28 04:39:39 +0000

    feat: update dpabi paramstyle to pyformat

--- spannerlib/wrappers/spannerlib-python/google-cloud-spanner-driver/google/cloud/spanner_driver/dbapi.py
+++ spannerlib/wrappers/spannerlib-python/google-cloud-spanner-driver/google/cloud/spanner_driver/dbapi.py
@@ -12,10 +12,32 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 """DBAPI 2.0 Global Variables."""
+
+# Globals: apilevel
+# String constant stating the supported DB API level.
+# https://peps.python.org/pep-0249/#apilevel
+# Currently only the strings “1.0” and “2.0” are allowed.
+# If not given, a DB-API 1.0 level interface should be assumed.
 apilevel: str = "2.0"
 
-# 1 = Threads may share the module, but not connections.
-# 2 = Threads may share the module and connections.
+# Globals: threadsafety
+# https://peps.python.org/pep-0249/#threadsafety
+# Integer constant stating the level of thread safety the interface supports.
+# Possible values are:
+# 0 - Threads may not share the module.
+# 1 - Threads may share the module, but not connections.
+# 2 - Threads may share the module and connections.
+# 3 - Threads may share the module, connections and cursors.
 threadsafety: int = 1
 
-paramstyle: str = "named"
+# Globals: paramstyle
+# https://peps.python.org/pep-0249/#paramstyle
+# String constant stating the type of parameter marker formatting expected
+# by the interface.
+# Possible values are
+# qmark - Question mark style, e.g. ...WHERE name=?
+# numeric - Numeric, positional style, e.g. ...WHERE name=:1
+# named - Named style, e.g. ...WHERE name=:name
+# format - ANSI C printf format codes, e.g. ...WHERE name=%s
+# pyformat - Python extended format codes, e.g. ...WHERE name=%(name)s
+paramstyle: str = "pyformat"
